42000d82a56a176a74877c89636a5d800c5c85b9
Ssun_0 <lalala1211@skuniv.ac.kr>
2020-05-07 00:09:30 +0900

    Jpa Auditing 완료

---
+++ src/main/java/com/ssun/cardquiz/springboot/domain/BaseTimeEntity.java
@@ -0,0 +1,23 @@
+package com.ssun.cardquiz.springboot.domain;
+//모든 Entity들의 상위 클래스가 되어 Entit들의 createdDate, modifedDate를 자동으로 관리하는 역할
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
--- src/test/java/com/ssun/cardquiz/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/ssun/cardquiz/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,7 +1,7 @@
-package com.ssun.web.CardQuiz.domain.posts;
+package com.ssun.cardquiz.springboot.domain.posts;
 
-import com.ssun.web.cardQuiz.domain.posts.Posts;
-import com.ssun.web.cardQuiz.domain.posts.PostsRepository;
+import com.ssun.cardquiz.springboot.domain.posts.Posts;
+import com.ssun.cardquiz.springboot.domain.posts.PostsRepository;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -42,6 +44,27 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2020, 5, 7, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>> createDate="+posts.getCreatedDate()+",modifiedDate="+posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 
 
