0d764176eff94bb062fdbf53d32aa85eb5785233
Ssun_0 <lalala1211@skuniv.ac.kr>
2020-06-05 16:53:52 +0900

    Google login 완료

---
+++ src/main/java/com/ssun/cardquiz/springboot/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.ssun.cardquiz.springboot.config;
+
+import com.ssun.cardquiz.springboot.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers){
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
+}
---
+++ src/main/java/com/ssun/cardquiz/springboot/config/auth/LoginUser.java
@@ -0,0 +1,12 @@
+package com.ssun.cardquiz.springboot.config.auth;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginUser { //이 파일을 어노테이션 클래스로 지정
+    //LoginUser라는 이름을 가진 어노테이션이 생성되었다고 보면 됨
+}
